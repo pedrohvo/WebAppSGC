69ba7675157a44e420c3ffd9de668241c3d7a888
Pedro Henrique <pedro.oliveira@solutis.com.br>
2018-09-10 15:22:25 -0300

    Adicionar arquivos de projeto.

---
+++ src/SGC.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.ApplicationCore.Entity
+{
+    class Cliente
+    {
+
+        public Cliente() { }
+
+        public int ClienteId { get; set; }
+        public String Nome { get; set; }
+        public int CPF { get; set; }
+
+    }
+}
